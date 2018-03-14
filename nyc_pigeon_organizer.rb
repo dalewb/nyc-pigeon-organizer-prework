d08057b550e59b6d870116da9d566511ec1e0730
William Dale <williambdale@gmail.com>
2018-03-14 17:14:44 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -7,7 +7,9 @@ def nyc_pigeon_organizer(pigeon_data)
       pigeon_data[attribute].each do |key,value|
         value.each do |name|
           # binding.pry
-          if pigeons.keys.include?(name) && pigeons[name][attribute]
+          if pigeons
+            
+          elsif pigeons.keys.include?(name) && pigeons[name][attribute]
             pigeons[name][attribute] << key 
             binding.pry
           else 
