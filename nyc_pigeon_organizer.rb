1a45eb9a5e508c5fba4fa5cdff53b32584870511
William Dale <williambdale@gmail.com>
2018-03-14 15:00:21 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,26 +1,3 @@
 require 'pry'
 
 
-def assignment 
-  if pigeons.keys.include?(name)
-    binding.pry 
-    pigeons[name][label] << info 
-  else 
-    pigeons[name] = {}
-    pigeons[name][label] = [info]
-  end 
-end 
-
-def nyc_pigeon_organizer(data)
-  pigeons = {}
-  data.each do |label, bird_data|
-    label.each do |factor|
-      binding.pry
-      assignment
-    end 
-  end 
-  binding.pry
-  pigeons 
-end
-
-
