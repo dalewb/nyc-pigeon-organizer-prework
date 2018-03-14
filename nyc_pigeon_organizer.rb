530098d8a1f241516d7b65fcbb2b87442454569a
William Dale <williambdale@gmail.com>
2018-03-14 15:28:48 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,8 +3,9 @@ require 'pry'
 def nyc_pigeon_organizer(pigeon_data)
   pigeon_data.each do |attributes, info|
     pigeon_data.keys.each do |attribute|
-      # pigeon_data[attribute]
-      binding.pry 
+      pigeon_data[attribute].each do |key,value|
+        binding.pry 
+      end 
     end 
   end 
 end 
